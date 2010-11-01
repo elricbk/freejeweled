03792040a8f4637ce563700dec4971d00289d202
elricbk <elricbk@gmail.com>
2010-11-01 23:02:06 +0000

    * Small fix to make sure that QApplication and QDeclarativeView show same version of application
    git-svn-id: svn+ssh://svn.code.sf.net/p/freejeweled/code@64 b33cdf50-4400-454d-b5a6-162f99772722

--- FreeJeweled/main.cpp
+++ FreeJeweled/main.cpp
@@ -7,16 +7,18 @@
 
 int main(int argc, char *argv[])
 {
+    QString appVersion = "0.7.0";
+
     QApplication a(argc, argv);
     a.setApplicationName("FreeJeweled");
-    a.setApplicationVersion("0.7");
+    a.setApplicationVersion(appVersion);
     a.setWindowIcon(QIcon("pics/appIcon.small.png"));
     qmlRegisterType<GemCell>("com.mycompany.gemcell", 1, 0, "GemCell");
     qmlRegisterType<GameBoard>("com.mycompany.gemcell", 1, 0, "GameBoard");
 
     QDeclarativeView view;
     g_mainEngine = view.engine();
-    view.engine()->rootContext()->setContextProperty("g_appVersion", "0.7.0");
+    view.engine()->rootContext()->setContextProperty("g_appVersion", appVersion);
     view.setSource(QUrl::fromLocalFile("MainForm.qml"));
 //    view.setMinimumSize(320, 480);
 //    view.setMaximumSize(320, 480);
